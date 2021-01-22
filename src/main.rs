85afc1223d4b04fd8d24bea6d475304aab019f5f
jschpp <darkdiscover@gmail.com>
2021-01-22 15:40:12 +0000

    feat: Input will now be read from STDIN and first line is starting to get parsed

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,28 @@
+use std::io;
+use regex::Regex;
+
+fn first_line(line: &String) {
+    let re = Regex::new(r"(fix|feat|build|docs|chore)!?(\(.*?\))?: .+").unwrap();
+    assert!(re.is_match(line));
+}
+
 fn main() {
-    println!("Hello, world!");
+    let mut line_count = 0;
+    let mut lastline = String::new();
+    loop {
+        let mut input = String::new();
+        io::stdin()
+            .read_line(&mut input)
+            .expect("failed to read from pipe");
+        input = input.trim().to_string();
+        if input == "" {
+            break;
+        }
+        if line_count == 0 {
+            first_line(&input);
+        }
+        println!("Pipe output: {}", input);
+        line_count += 1;
+        lastline = input;
+    }
 }
